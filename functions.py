1ee2b9a6ef875300cf0debdeb16d33ec25c23e72
Helios De Rosario <helios.derosario@gmail.com>
2024-02-24 23:32:21 +0100

    refactor

--- functions.py
+++ functions.py
@@ -24,35 +24,42 @@ def priorizaregex(patterns):
     priorizadas.extend(interiores)
     return priorizadas
 
-def addmatches(oldmatches, newmatches):
+def addreplacements(oldrep, newrep):
     """
-    Añadir tuplas de "Matches" acompañadas con las cadenas de reemplazo a una lista,
-    seleccionando solo los casos en los que los "Matches" no se solapan.
+    `oldrep` y `newrep` son listas de tuplas `(p, r)` formadas por
+    un rango de posiciones `p` y una cadena de texto de reemplazo `r`.
+    Esta función amplía `oldrep` añadiendo los casos de `newrep`
+    en los que los rangos no se solapan.
     """
-    candidatos = [m for m in newmatches] # copiar lista
-    for (m, r) in candidatos.copy():
-        first, last = m.span()
-        for o in oldmatches:
-            a, b = o[0].span()
-            if (a <= first < b) or (a < last <= b): # overlaps
-                candidatos.remove((m, r))
+    candidatos = [m for m in newrep] # copiar lista
+    for (p, r) in candidatos.copy():
+        first, last = p
+        for rep in oldrep:
+            a, b = rep[0]
+            if (a <= first < b) or (a < last <= b): # solapamiento
+                candidatos.remove((p, r))
                 break
-    oldmatches.extend(candidatos)
+    oldrep.extend(candidatos)
 
-def reemplazar(texto, reemplazos):
-    patterns = priorizaregex(reemplazos.keys())
-    matches = []
-    for pat in patterns:
-        newmatches = [m for m in re.finditer(pat, texto)]
-        newreplacements = [m.expand(reemplazos[pat]) for m in newmatches]
-        addmatches(matches, zip(newmatches, newreplacements))
-    if len(matches) == 0:
+def reemplazar(texto, dicregex):
+    """
+    Modifica `texto` aplicando los reemplazos por expresiones regulares
+    definidos en el diccionario `dicregex`.
+    """
+    patrones = priorizaregex(dicregex.keys())
+    reemplazos = []
+    for pat in patrones:
+        matches = [m for m in re.finditer(pat, texto)]
+        newrep = [(m.span(), m.expand(dicregex[pat])) for m in matches]
+        addreplacements(reemplazos, newrep)
+    if len(reemplazos) == 0:
         return texto
-    matches.sort(key=lambda mr: mr[0].span()[0]) # ordenar
+    reemplazos.sort(key=lambda mr: mr[0][0]) # ordenar
+    # montar texto
     tokens = []
     posicion = 0
-    for (m, r) in matches:
-        first, last = m.span()
+    for (m, r) in reemplazos:
+        first, last = m
         if posicion < first:
             tokens.append(texto[posicion:first])
         tokens.append(r)
@@ -70,12 +77,11 @@ def maptengwar(texto):
     for (w, r) in rules["words"].items():
         dic["(^|[^\\w])"+w+"($|[^\\w])"] = "\\1"+r+"\\2"
     dic.update(rules["map"])
-    return reemplazar(texto, dic)
-
-def fixtengwar(tengwar):
+    tengwar = reemplazar(texto, dic)
     # vocales sueltas
     if tengwar.startswith("["):
         tengwar = "{telco}" + tengwar
+    # otros ajustes para hacer usable en Tecendil
     tengwar = tengwar.replace(" [", " {telco}[")
     tengwar = tengwar.replace("{}[", "{telco}[")
     tengwar = tengwar.replace("{}", "")
@@ -89,7 +95,6 @@ def encode(texto):
 def transcribe(texto, font="telcontar"):
     texto = preprocess(texto)
     texto = maptengwar(texto)
-    texto = fixtengwar(texto)
     if font == "telcontar":
         texto = encode(texto)
     return texto
